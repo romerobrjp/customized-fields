0c6b12c891372d4806829263e8b80b4fe66ea752
Romero Meireles <romero.mfm@gmail.com>
2016-06-14 02:28:51 -0300

    Routes config for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+  resources :fields
+  resources :contacts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'contacts#index'
+  devise_for :users
+  #devise_for :users, :controllers => {sessions: "users/sessions"}
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
