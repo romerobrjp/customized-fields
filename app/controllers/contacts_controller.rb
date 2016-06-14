94b47b992dba20209f3c4783da251fb670676e37
Romero Meireles <romero.mfm@gmail.com>
2016-06-14 02:28:24 -0300

    Controllers initial config;

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,54 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:show, :edit, :update, :destroy]
+  before_filter :require_authentication
+
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def show
+  end
+
+  def create
+    # @contact = Contact.new(contact_params)
+    #
+    # if @contact.save
+    #   redirect_to @contact, notice: "Contact created successfully"
+    # else
+    #   render action: :new, notice: "Could not save this contact"
+    # end
+  end
+
+  def edit
+  end
+
+  def update
+    # if @contact.update(contact_params)
+    #   redirect_to @contact, notice: "Contact updated successfully"
+    # else
+    #   redirect_to @contact, notice: "Could not update this contact"
+    # end
+  end
+
+  def destroy
+    if @contact.destroy
+      redirect_to @contact, notice: "Contact removed successfully"
+    else
+      redirect_to @contact, notice: "Contact could not be removed"
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :type, :description)
+  end
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+end
