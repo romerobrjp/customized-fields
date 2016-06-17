ca86229fc7f58630a9f34898e213ff862f940f78
Romero Meireles <romero.mfm@gmail.com>
2016-06-16 23:10:13 -0300

    enum values in english

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,5 +2,5 @@ class Field < ActiveRecord::Base
   belongs_to :user
   has_many :field_values, dependent: :destroy
   validates_presence_of :name, :field_type
-  enum types: {text_field: 'Texto', text_area: 'Área de texto', combobox: 'Combobox'}
+  enum types: {text_field: 'Text', text_area: 'Text Area', combobox: 'Combobox'}
 end
